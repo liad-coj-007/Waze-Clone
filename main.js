5c2fbe9634d9f3e1c283b95f9d076352d03138ed
liad-coj-007 <liadc@campus.technion.ac.il>
2025-08-26 15:33:54 +0300

    adding a point push

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,3 +10,17 @@ osm.addTo(map);
 
 var marker = L.marker([32.07, 34.79]).addTo(map);
 marker.bindPopup("vertex").openPopup();
+
+function getPos(pos){
+  var str = "Lat: " + pos.lat + "\nLon: " + pos.lng;
+  return str;
+}
+
+function onMapClick(e){
+    var str = getPos(e.latlng);
+    var pos = [e.latlng.lat,e.latlng.lng];
+    var marker = L.marker(pos).addTo(map);
+    marker.bindPopup(str).openPopup();
+}
+
+map.on('click', onMapClick);
