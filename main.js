fe6dd98b0350335491d6f7f47d9a4553ae932d14
liad-coj-007 <liadc@campus.technion.ac.il>
2025-08-26 15:17:23 +0300

    init the leafjs

---
+++ main.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+osm.addTo(map);
+
+var marker = L.marker([32.07, 34.79]).addTo(map);
+marker.bindPopup("vertex").openPopup();
