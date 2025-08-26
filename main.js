5b55f8ee48907df401e09615f8746b8f3f7ce813
liad-coj-007 <liadc@campus.technion.ac.il>
2025-08-26 16:11:09 +0300

    adding a point

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+import {Point } from "./Point.js";
+
+//colors 
+// ===============================================================
+var redIcon = L.icon({
+  iconUrl: 'https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers@master/img/marker-icon-red.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var greenIcon = L.icon({
+  iconUrl: 'https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers@master/img/marker-icon-green.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+// ========================================================
 
 var map = L.map('map').setView([51.505, -0.09], 13);
+
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,19 +31,7 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 osm.addTo(map);
 
-var marker = L.marker([32.07, 34.79]).addTo(map);
-marker.bindPopup("vertex").openPopup();
-
-function getPos(pos){
-  var str = "Lat: " + pos.lat + "\nLon: " + pos.lng;
-  return str;
-}
-
-function onMapClick(e){
-    var str = getPos(e.latlng);
-    var pos = [e.latlng.lat,e.latlng.lng];
-    var marker = L.marker(pos).addTo(map);
-    marker.bindPopup(str).openPopup();
-}
-
-map.on('click', onMapClick);
+var start_point = new Point(L.marker([32.07, 34.79], { draggable: true,icon: greenIcon}).addTo(map),"start");
+start_point.Popup();
+var end_point = new Point(L.marker([31.9, 34.79], { draggable: true,icon: redIcon}).addTo(map),"finish");
+end_point.Popup();
